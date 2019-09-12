6b5dabad5c98a193ff21b1489dd64580fbcf72dc
clever-minifier-2555 <alisabaj@gmail.com>
2019-09-12 16:08:06 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  counter = 0
+  new_array = []
+
+  while counter < array.length do
+    new_array << array[counter] * array[counter]
+    counter += 1
+  end
+
+  new_array
 end
